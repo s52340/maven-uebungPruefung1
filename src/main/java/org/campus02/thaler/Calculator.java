1d5e45fd1dcf173f9a79510b2c25738f8bfb4389
bthaler <bianca.thaler@gmx.net>
2023-02-07 01:17:00 +0100

    Testklasse fertiggestellt, mvn - mit dependencies(junit) und plugin(surefire)

--- src/main/java/org/campus02/thaler/Calculator.java
+++ src/main/java/org/campus02/thaler/Calculator.java
@@ -1,2 +1,37 @@
-package org.campus02.thaler;public class Calculator {
+package org.campus02.thaler;
+
+public class Calculator {
+
+    private int a;
+    private int b;
+
+    public Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    private int sumVariables(){
+       int c = a + b;
+       return c;
+    }
+
+    public String result(){
+        int sum_AB = sumVariables();
+        if (sum_AB < 0){
+            return "unter Null";
+        } else if (sum_AB < 10) {
+            return "zwischen 0 und 10";
+        }
+        else {
+            return "ueber 10";
+        }
+    }
 }
--- src/test/java/org/campus02/thaler/CalculatorTest.java
+++ src/test/java/org/campus02/thaler/CalculatorTest.java
@@ -1,4 +1,33 @@
+package org.campus02.thaler;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorTest {
-  
+
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+    calculator = new Calculator(4,5);
+    }
+
+    @Test
+    @DisplayName("Konstruktor Test")
+    void testConstruktor(){
+        int expectedNumber = 4;
+        Assertions.assertEquals(expectedNumber,calculator.getA());
+
+        Assertions.assertEquals(5, calculator.getB());
+    }
+
+    @Test
+    void result() {
+        String expected = "zwischen 0 und 10";
+        Assertions.assertEquals(expected,calculator.result());
+    }
 }
